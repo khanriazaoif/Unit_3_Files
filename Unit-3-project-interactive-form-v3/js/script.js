55acbe9770f29e0f51d4720737c66dcbea0afe65
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-01-17 22:24:07 -0600

    Unit 3 Project step 4 complete

--- Unit-3-project-interactive-form-v3/js/script.js
+++ Unit-3-project-interactive-form-v3/js/script.js
@@ -1,7 +1,16 @@
 const nameField = document.getElementById('name');
 const jobRole = document.getElementById('other-job-role');
+const title = document.getElementById('title');
+
 
 nameField.focus();
 jobRole.style.display = 'none';
 
-console.log(jobRole);
+title.addEventListener('change', () => {
+    if (title.value === 'other') {
+        jobRole.style.display = 'block';
+        console.log('other');
+    }
+});
+
+// console.log(jobRole);
