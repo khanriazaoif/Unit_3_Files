5630e11a1ad2f59b708ac790abf8e3dca83c046e
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-01-17 23:23:39 -0600

    Unit 3 Project step 5 start

--- Unit-3-project-interactive-form-v3/js/script.js
+++ Unit-3-project-interactive-form-v3/js/script.js
@@ -1,7 +1,9 @@
 const nameField = document.getElementById('name');
 const jobRole = document.getElementById('other-job-role');
 const title = document.getElementById('title');
-
+const color = document.getElementById('color');
+const design = document.getElementById('design');
+// const coloredShirts = color.getAttribute('color');
 
 nameField.focus();
 jobRole.style.display = 'none';
@@ -13,4 +15,12 @@ title.addEventListener('change', () => {
     }
 });
 
-// console.log(jobRole);
+color.disabled = true;
+design.addEventListener('change', () => {
+    color.disabled = false;
+    if (design.value === 'js puns') {
+        console.log('js puns');
+        let coloredShirts = color.getAttribute('data-theme');
+        console.log(coloredShirts);
+    }
+});
