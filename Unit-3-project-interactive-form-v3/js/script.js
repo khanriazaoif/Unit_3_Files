32628c014d260709427d184e5b0aa8ee9cba2e4b
Riaz Khan <37626809+khanriazaoif@users.noreply.github.com>
2023-01-17 22:02:07 -0600

    Unit 3 Project step 4 v1

--- Unit-3-project-interactive-form-v3/js/script.js
+++ Unit-3-project-interactive-form-v3/js/script.js
@@ -1,2 +1,7 @@
 const nameField = document.getElementById('name');
+const jobRole = document.getElementById('other-job-role');
+
 nameField.focus();
+jobRole.style.display = 'none';
+
+console.log(jobRole);
